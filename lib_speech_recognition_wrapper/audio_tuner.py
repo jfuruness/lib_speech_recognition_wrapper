1c5dd9bd5ad14ae81295a960bf29998f328572f3
Justin Furuness <jfuruness@gmail.com>
2021-01-26 09:47:54 -0500

    testing

--- lib_speech_recognition_wrapper/audio_tuner.py
+++ lib_speech_recognition_wrapper/audio_tuner.py
@@ -1,11 +1,12 @@
 import os
 import logging
-from multiprocessing import Manager
+from multiprocessing import Manager, cpu_count
 
 from pathos.multiprocessing import ProcessPool
 from pocketsphinx import get_model_path
 import pyaudio
 import tarfile
+from shutil import copyfile
 import wave
 
 from lib_utils import utils
@@ -19,6 +20,8 @@ class Audio_Tuner:
     tuned_path = "/etc/tuned"
     transcription_name = "assistant.transcription"
     file_ids_name = "assistant.fileids"
+    test_transcription_name = "test.transcription"
+    test_file_ids_name = "test.fileids"
 
     def __init__(self, tuning_phrases: list, times_to_record=1, test=False):
         """tuning phrases to be tuned to"""
@@ -31,12 +34,23 @@ class Audio_Tuner:
         self.model_path = get_model_path()
 
     def run(self):
+        self.generate_new_model()
+        self.test_new_model()
+
+    def generate_new_model(self)
         self.write_files()
         self.record_files()
         self.copy_files()
         self.install_sphinx_base()
         self.run_sphinx_fe()
         self.download_proper_en()
+        self.convert_mdef()
+        self.download_sphinxtrain()
+        self.run_bw()
+        self.run_mllr()
+
+    def test_new_model(self):
+        self.write_test_files()
         input("wait")        
 
     def write_files(self):
@@ -52,7 +66,6 @@ class Audio_Tuner:
     def copy_files(self):
         # Using bash instead of python to closely follow directions on
         # https://cmusphinx.github.io/wiki/tutorialadapt/
-        input(self.model_path)
         for _dir in ["en-us",
                      "cmudict-en-us.dict",
                      "en-us.lm.bin"]:
@@ -73,7 +86,7 @@ class Audio_Tuner:
                         f"git clone {url}",
                         "cd sphinxbase",
                         "./autogen.sh",
-                        "make",
+                        f"make -j {cpu_count()}",
                         "sudo make install",
                         f"cp src/sphinx_fe/sphinx_fe {self.tuned_path}"])
 
@@ -87,12 +100,95 @@ class Audio_Tuner:
         url = ("https://phoenixnap.dl.sourceforge.net/project/cmusphinx/"
                "Acoustic%20and%20Language%20Models/US%20English/"
                "cmusphinx-en-us-5.2.tar.gz")
-        path = os.path.join(self.tuned_path, "larger_sphinx")
+        path = os.path.join(self.tuned_path, "larger_sphinx.tar.gz")
         utils.download_file(url, path)
         with tarfile.open(path) as f:
             old_en_us_path = os.path.join(self.tuned_path, "en-us")
             utils.delete_paths(old_en_us_path)
             f.extractall(old_en_us_path)
+            utils.run_cmds([f"cd {old_en_us_path}",
+                            f"mv * old_folder",
+                            "cd old_folder",
+                            "mv * ..",
+                            "rm -rf old_folder"])
+
+    def convert_mdef(self):
+        utils.run_cmds("sudo apt -y install pocketsphinx")
+        path = os.path.join(self.tuned_path, "en-us/mdef")
+        utils.run_cmds(f"pocketsphinx_mdef_convert -text {path} {path}.txt")
+
+    def download_sphinxtrain(self):
+        # Must get installed from source for fixes
+        utils.run_cmds([f"cd {self.tuned_path}",
+                        "git clone git@github.com:cmusphinx/sphinxtrain.git",
+                        "cd sphinxtrain",
+                        "./autogen.sh",
+                        f"make -j {cpu_count()}",
+                        "sudo make install"])
+        for fname in ["bw", "map_adapt", "mk_s2sendump", "mllr_solve"]:
+            old_path = os.path.join("/usr/local/libexec/sphinxtrain/", fname)
+            new_path = os.path.join(self.tuned_path, fname)
+            utils.run_cmds(f"cp {old_path} {new_path}")
+        
+    def run_bw(self):
+        utils.run_cmds([f"cd {self.tuned_path}",
+                        ("sudo ./bw \\\n"
+                         " -hmmdir en-us \\\n"
+                         " -moddeffn en-us/mdef.txt \\\n"
+                         #"-ts2cbfn .ptm. \\\n"
+                         " -ts2cbfn .cont. \\\n"
+                         " -feat 1s_c_d_dd \\\n"
+                         #"-svspec 0-12/13-25/26-38 \\\n"
+                         " -lda en-us/feature_transform \\\n"
+                         " -cmn current \\\n"
+                         " -agc none \\\n"
+                         " -dictfn cmudict-en-us.dict \\\n"
+                         f" -ctlfn {self.file_ids_name} \\\n"
+                         f" -lsnfn {self.transcription_name} \\\n"
+                         " -accumdir .")])
+
+    def run_mllr(self):
+        utils.run_cmds([f"cd {self.tuned_path}",
+                        ("./mllr_solve\\\n"
+                         " -meanfn en-us/means \\\n"
+                         " -varfn en-us/variances \\\n"
+                         " -outmllrfn mllr_matrix -accumdir .")])
+
+    def write_test_files(self):
+        utils.makedirs(self.test_dir)
+        wav_dir = os.path.join(self.test_dir, "wav/")
+        utils.makedirs(wav_dir)
+        old_lm = os.path.join(self.tuned_path, "en-us.lm.bin")
+        new_lm = os.path.join(self.test_dir, "en-us.lm.bin")
+        old_dict = os.path.join(self.tuned_path, "cmudict-en-us.dict")
+        new_dict = os.path.join(self.test_dir, "cmudict-en-us.dict")
+        old_hmm = os.path.join(self.tuned_path, "en-us")
+        new_hmm = os.path.join(self.test_dir, "en-us")
+        old_mllr_matrix = os.path.join(self.tuned_path, "mllr_matrix"),
+        new_mllr_matrix = os.path.join(self.test_dir, "mllr_matrix")
+        utils.run_cmds([f"cd {self.tuned_path}",
+                        f"cp {self.file_ids_path} {self.test_file_ids_path}",
+                        (f"cp {self.transcription_path} "
+                         f"{self.test_transcription_path}"),
+                        f"cp *wav {wav_dir}",
+                        f"cp {old_lm} {new_lm}",
+                        f"cp {old_dict} {new_dict}",
+                        f"cp {old_hmm} {new_hmm}",
+                        f"cp {old_mllr_matrix} {new_mllr_matrix}"])
+
+    def run_decoder(self):
+        for mllr in "", "\\\n -mllr mllr_matrix":
+            utils.run_cmds((f"cd {self.test_dir} && \\\n",
+                            "pocketsphinx_batch \\\n"
+                            f" -adcin yes \\\n"
+                            f" -cepdir wav \\\n"
+                            f" -cepext .wav \\\n"
+                            f" -ctl test.fileids \\\n"
+                            f" -lm `en-us.lm.bin` \\\n"
+                            f" -dict `cmudict-en-us.dict` \\\n"
+                            f" -hmm `en-us` \\\n"  # for example en-us
+                            f" -hyp test.hyp") + mllr)
+
 
     def write_transcription_file_ids(self):
         with open(self.transcription_path, "w") as transcription:
@@ -154,3 +250,15 @@ class Audio_Tuner:
     @property
     def file_ids_path(self):
         return os.path.join(self.tuned_path, self.file_ids_name)
+
+    @propert
+    def test_dir(self):
+        return os.path.join(self.tuned_path, "test")
+
+    @property
+    def test_transcription_path(self):
+        return os.path.join(self.test_dir, self.test_transcription_name)
+
+    @property
+    def test_file_ids_path(self):
+        return os.path.join(self.test_dir, self.test_file_ids_name)
